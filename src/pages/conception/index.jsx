8f1628e7b772a09f43108c69b89f9fba2830bd92
Ibrahima <ibracool99@gmail.com>
2022-03-17 23:47:27 +0100

    chore: conception images

--- src/pages/conception/index.jsx
+++ src/pages/conception/index.jsx
@@ -17,7 +17,7 @@ const Renovate = () => {
                 ]}
             />
             <GuideProcess></GuideProcess>
-            <Card linkTo={'quote'} title={"Votre construction de maison sereinement avec un architecte"} showButton buttonTitle="Confier un devis">
+            <Card imgSrc={"/assets/img/conception/21.jpg"} linkTo={'quote'} title={"Votre construction de maison sereinement avec un architecte"} showButton buttonTitle="Confier un devis">
                 Chez Sedar, nous sommes convaincus que votre projet sera un succès uniquement si vous avez anticipé les prix, les délais et les étapes de votre rénovation. En présentant votre projet sur Sedar, nos experts habitats vous donnent gratuitement le coup d’avance sur vos travaux. Nous vous accompagnons des premiers plans au dernier coup de peinture avec nos architectes et entreprises triés sur le volet.
                 <p><b>Pourquoi faire confiance à Sedar pour votre projet ?</b></p>
                 <ol>
@@ -28,7 +28,7 @@ const Renovate = () => {
                     <li> <b>- </b>Garantie de bonne fin de chantier AXA </li>
                 </ol> Ici, nous partageons notre savoir sous la forme d’un dossier complet sur la réhabilitation afin que vous ayez accès à une information fiable.
             </Card>
-            <Card title={"Quels sont les prix au m² pour la construction d’une maison neuve ?"} imgLeft animation={"fadeInLeft"} showButton linkTo={'quote'} buttonTitle={"Demander une estimation personnalisée"}>
+            <Card imgSrc={"/assets/img/conception/22.jpg"} title={"Quels sont les prix au m² pour la construction d’une maison neuve ?"} imgLeft animation={"fadeInLeft"} showButton linkTo={'quote'} buttonTitle={"Demander une estimation personnalisée"}>
                 De la conception à la remise des clés, en passant par l’obtention du permis de construire, plusieurs étapes vont succéder pour la construction de votre maison. S’il est difficile d’établir précisément le coût de construction d’une maison, on peut estimer un budget global pour chacune des étapes de la construction, tel le gros œuvre, le second œuvre et les finitions.
                 Le coût de construction d’une maison individuelle variera ainsi en fonction du type de construction, l’emplacement, la taille et les types de matériaux. Pour que vous ayez accès aux prix transparents dans la construction de maison, voici les moyennes réelles du marché:
                 <ol><li> <b>- 1000€ TTC par m2 </b> pour une construction en gros oeuvre</li>
@@ -38,7 +38,7 @@ const Renovate = () => {
                 </ol>Chez Sedar, nous pensons que chaque projet mérite un architecte. Il faut savoir cependant que les honoraires des architectes sont entre 3% à 10% du coût de la construction et contrairement aux idées reçues, ces coûts ne sont pas plus élevés que lorsque l’on fait appel à un constructeur de maisons individuelles.
                 L’un des services les plus essentiels apportés par l’architecte consiste à adapter le projet au budget. Pour cela, il réalise un appel d’offres aux différents entreprises générales du bâtiment ou artisans afin de connaître leur prix pour chaque étape de la construction, et optimise les dépenses en fonction.
             </Card>
-            <Card title={"Construction de maison neuve : sur quoi se reposent les postes de coûts ?"} animation={"fadeInRight"} >
+            <Card imgSrc={"/assets/img/conception/23.jpg"} title={"Construction de maison neuve : sur quoi se reposent les postes de coûts ?"} animation={"fadeInRight"} >
                 <p> <b>Terrain et excavation</b> : Préparer le terrain pour la construction est une première étape importante, là le prix veut fortement varier. Dans la plupart des projets, le terrain est déjà préparé pour la construction. Si vous avez acheté votre propre lot, vous devrez peut-être embaucher un pro pour l’excavation.
                 </p><p><b>Taille de la maison</b> : Une maison avec une plus grande superficie coûtera plus chère qu’une maison plus petite.
                 </p><p> <b> Forme </b>: Plus la maison demande une forme particulière, plus elle sera chère. Cependant, les formes inhabituelles sont plus fréquentes dans les conceptions personnalisées.
